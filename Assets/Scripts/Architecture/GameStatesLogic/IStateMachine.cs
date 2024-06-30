da4bd5d4343381527930b172f938369bff07ac01
Querty12344 <102682104+Querty12344@users.noreply.github.com>
2024-06-30 04:49:50 +0300

    Small CleanUp

--- Assets/Scripts/Architecture/GameStatesLogic/IStateMachine.cs
+++ Assets/Scripts/Architecture/GameStatesLogic/IStateMachine.cs
@@ -1,45 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using Architecture.GameStatesLogic.States;
-
-namespace Architecture.GameStatesLogic
+﻿namespace Architecture.GameStatesLogic
 {
     public interface IStateMachine
     {
         void Enter<TState>() where TState : IState;
-    }
-
-    internal class StateMachine : IStateMachine
-    {
-        private IState _active;
-        private readonly Dictionary<Type, IState> _states;
-
-        public StateMachine(BootstrapState bootstrapState,
-            ClientConnectionState clientConnectionState,
-            ControllerWaitingState controllerWaitingState,
-            GameControllingState gameControllingState,
-            GameLoopState gameLoopState,
-            HostLeavingState leaveLevelState,
-            ModeChoosingState modeChoosingState,
-            LevelBuildingState levelBuildingState)
-        {
-            _states = new Dictionary<Type, IState>
-            {
-                [typeof(LevelBuildingState)] = levelBuildingState,
-                [typeof(BootstrapState)] = bootstrapState,
-                [typeof(ClientConnectionState)] = clientConnectionState,
-                [typeof(ControllerWaitingState)] = controllerWaitingState,
-                [typeof(GameControllingState)] = gameControllingState,
-                [typeof(GameLoopState)] = gameLoopState,
-                [typeof(HostLeavingState)] = leaveLevelState,
-                [typeof(ModeChoosingState)] = modeChoosingState
-            };
-        }
-
-        public void Enter<TState>() where TState : IState
-        {
-            _active = _states[typeof(TState)];
-            _active.Enter(this);
-        }
     }
 }
---
+++ Assets/Scripts/Architecture/GameStatesLogic/StateMachine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Architecture.GameStatesLogic.States;
+
+namespace Architecture.GameStatesLogic
+{
+    public class StateMachine : IStateMachine
+    {
+        private IState _active;
+        private readonly Dictionary<Type, IState> _states;
+
+        public StateMachine(BootstrapState bootstrapState,
+            ClientConnectionState clientConnectionState,
+            ControllerWaitingState controllerWaitingState,
+            GameControllingState gameControllingState,
+            GameLoopState gameLoopState,
+            HostLeavingState leaveLevelState,
+            ModeChoosingState modeChoosingState,
+            LevelBuildingState levelBuildingState)
+        {
+            _states = new Dictionary<Type, IState>
+            {
+                [typeof(LevelBuildingState)] = levelBuildingState,
+                [typeof(BootstrapState)] = bootstrapState,
+                [typeof(ClientConnectionState)] = clientConnectionState,
+                [typeof(ControllerWaitingState)] = controllerWaitingState,
+                [typeof(GameControllingState)] = gameControllingState,
+                [typeof(GameLoopState)] = gameLoopState,
+                [typeof(HostLeavingState)] = leaveLevelState,
+                [typeof(ModeChoosingState)] = modeChoosingState
+            };
+        }
+
+        public void Enter<TState>() where TState : IState
+        {
+            _active = _states[typeof(TState)];
+            _active.Enter(this);
+        }
+    }
+}
